11c4e414d6c932af5106541a105ec4942a4098cd
marina <golovina.marina@gmail.com>
2021-07-28 10:47:19 +1000

    Display Scheduled Works API endpoint data on the map

--- client/components/ScheduledWorks.jsx
+++ client/components/ScheduledWorks.jsx
@@ -4,10 +4,12 @@ import L from 'leaflet';
 import { usePosition } from 'use-position'
 import { getScheduledWorks } from '../api'
 
-export default function Header(props) {
+export default function Header() {
     const watch = true
     const { latitude, longitude } = usePosition(watch)
     const [worksPosition, setWorksPosition] = useState([])
+    const [scheduledWorks, setScheduledWorks] = useState([-36.8509, 174.7645])
+    const [map, setmap] = useState(null)
 
     // const greenIcon = new L.Icon({
     //     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
@@ -35,40 +37,48 @@ export default function Header(props) {
 
     function displayScheduledWorks(evt) {
         evt.preventDefault()
-        // getScheduledWorks()
-        // .then(works => {
-        //     console.log('Logging displayScheduledWorks function:', works.response[0].latitude)
-        //     // const recentWorks = []
-        //     // works.filter((function (position) {
-        //     //     if (!recentWorks.includes)
-        //     // }))
+        getScheduledWorks()
 
-        // })
-        return (
-            <MapContainer className='map-container' center={[-36.8509, 174.7645]} zoom={10} scrollWheelZoom={true} style={{ height: '400px', width: '400px' }}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[-36.9915, 174.8734]}></Marker>
-                {/* {works.response.map(work => <Marker position={[work.latitude, work.longitude]} key={work.id}></Marker>)} */}
-            </MapContainer>
-        )
+            .then(works => {
+                console.log('Logging displayScheduledWorks function:', [works.response[0]])
+                setScheduledWorks(works.response.map(work => <li key={work.id}>{[work.latitude, work.longitude]}</li>))
+                setmap(
+                    <div className='map' style={{ height: '400px', width: '100%' }}>
+                        <MapContainer className='map-container' center={[-36.8509, 174.7645]} zoom={10} scrollWheelZoom={true} style={{ height: '400px', width: '100%' }}>
+                            <TileLayer
+                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            {works.response.map(work => <Marker position={[work.latitude, work.longitude]} key={work.id}><Tooltip>{work.name}, {work.suburb}, {work.address}, {work.startDate},{work.lastUpdated}</Tooltip></Marker>)}
+                        </MapContainer>
+                    </div>
+                )
+
+                // const recentWorks = []
+                // works.filter((function (position) {
+                //     if (!recentWorks.includes)
+                // }))
+
+            })
+        // return (
+        //     <MapContainer className='map-container' center={[-36.8509, 174.7645]} zoom={10} scrollWheelZoom={true} style={{ height: '400px', width: '400px' }}>
+        //         <TileLayer
+        //             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        //             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        //         />
+        //         <Marker position={[-36.9915, 174.8734]}></Marker>
+        //         {/* {works.response.map(work => <Marker position={[work.latitude, work.longitude]} key={work.id}></Marker>)} */}
+        //     </MapContainer>
+        // )
     }
 
     return (
         <div className='scheduled-works'>
             <button onClick={displayScheduledWorks}>Show scheduled works in Auckland</button>
-            <h3>Display results on the map</h3>
-            <div className='map' style={{ height: '400px', width: '400px' }}>
-                <MapContainer className='map-container' center={[-36.8509, 174.7645]} zoom={10} scrollWheelZoom={true} style={{ height: '400px', width: '400px' }}>
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[-36.8509, 174.7645]}><Tooltip>This is just Auckland coordinates</Tooltip></Marker>
-                </MapContainer>
-            </div>
+            <div>{map}</div>
+            <ul>
+                {scheduledWorks}
+            </ul>
         </div>
     )
 }
